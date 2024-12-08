140f3040acc12249a167a279caab09961ee81fd8
Henrik Suurik <henrik.suurik@chasacademy.se>
2024-12-08 18:04:08 +0100

    Added Error Handling for JSON functions

--- js/ls.js
+++ js/ls.js
@@ -2,10 +2,19 @@
 const CART_NAME = "Shopping-Cart";
 
 export function getCartLocalStorage() {
-    const lsCart = localStorage.getItem(CART_NAME);
-    return lsCart ? JSON.parse(lsCart) : [];
+    try {
+        const lsCart = localStorage.getItem(CART_NAME);
+        return lsCart ? JSON.parse(lsCart) : [];
+    } catch(error) {
+        console.error(error);
+        return [];
+    }
 }
 
 export function saveCartLocalStorage(cartObj) {
-    localStorage.setItem(CART_NAME, JSON.stringify(cartObj));
+    try {
+        localStorage.setItem(CART_NAME, JSON.stringify(cartObj));
+    } catch(error) {
+        console.error(error);
+    }
 }
